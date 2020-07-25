462956ae16d66ed2426c0ef06b154ec538058eab
Jin Min Jun <gamejun16@daum.net>
2020-07-25 21:28:27 +0900

    1.0.0 first commit

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadSceneAsync("PlayerScene");
+        SceneManager.LoadSceneAsync("JombieScene", LoadSceneMode.Additive);
+    }
+
+    public void GameQuit()
+    {
+        Application.Quit();
+        
+    }
+}
