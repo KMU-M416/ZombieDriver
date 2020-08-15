749f8b8cd6c422580dfe37992236f599b77e9fd7
Jin Min Jun <gamejun16@daum.net>
2020-08-16 02:00:55 +0900

    1.0.13 Apply NPC Spawner, etc.

---
+++ Assets/Scripts/PlayerScene/NPCTaker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCTaker : MonoBehaviour
+{
+    public Transform[] seat;
+    
+
+    // NPC와의 직접 충돌을 통해 해당 NPC를 획득하는 구문
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("NPC"))
+        {
+            collision.transform.GetComponentInParent<NPCInfo>().SitOnTruck();
+            int i = collision.transform.GetComponentInParent<NPCInfo>().seatIdx;
+
+            Transform npc = collision.transform.root;
+
+            // 본 CollisionEnter가 두 번 반복되는 현상 존재
+            // Truck에 탑승한 상태에서 본 구문이 반복되며 발생되는 버그를 방지하기 위함
+            if (npc.gameObject.layer != LayerMask.NameToLayer("NPC")) return;
+            
+            npc.parent = seat[i];
+            npc.localPosition = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScene/Npc/Spawner/PlayerWaitChecker.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class PlayerWaitChecker : MonoBehaviour
+{
+    Image waitTimeViewer, waitTimeViewerBackground;
+    float waitTime;
+    float curWaitTime = 0;
+
+    private void Start()
+    {
+        waitTime = GetComponentInParent<NpcSpawnerController>().waitTime;
+
+        waitTimeViewer = transform.Find("Canvas").GetChild(1).GetComponent<Image>();
+        waitTimeViewerBackground = transform.Find("Canvas").GetChild(0).GetComponent<Image>();
+        waitTimeViewer.gameObject.SetActive(false);
+        waitTimeViewerBackground.gameObject.SetActive(false);
+
+        waitTimeViewer.transform.eulerAngles = new Vector3(45, 45, 0);
+        waitTimeViewerBackground.transform.eulerAngles = new Vector3(45, 45, 0);
+        waitTimeViewer.fillAmount = 0f;
+    }
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if(other.CompareTag("Player"))
+        {
+            waitTimeViewer.gameObject.SetActive(true);
+            waitTimeViewerBackground.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if(other.CompareTag("Player"))
+        {
+            curWaitTime += Time.deltaTime;
+            waitTimeViewer.fillAmount = curWaitTime / waitTime; 
+
+            if(curWaitTime >= waitTime)
+            {
+                GetComponentInParent<NpcSpawnerController>().GetNpcFromSpawner();
+            }
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        // 대기 시간 초기화
+        //if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if(other.CompareTag("Player"))
+        {
+            curWaitTime = 0;
+            waitTimeViewer.gameObject.SetActive(false);
+            waitTimeViewerBackground.gameObject.SetActive(false);
+
+        }
+    }
+}
